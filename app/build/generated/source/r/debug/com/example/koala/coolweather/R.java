75bb3d66bb18f51154f0d21db96bac2ff30e1848
Koala <nwulx@126.com>
2016-03-10 09:42:26 +0800

    修改程序图标和名称

--- app/build/generated/source/r/debug/com/example/koala/coolweather/R.java
+++ app/build/generated/source/r/debug/com/example/koala/coolweather/R.java
@@ -1594,8 +1594,9 @@ containing a value of this type.
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020043;
         public static final int abc_textfield_search_material=0x7f020044;
         public static final int home=0x7f020045;
-        public static final int notification_template_icon_bg=0x7f020047;
-        public static final int refresh=0x7f020046;
+        public static final int logo=0x7f020046;
+        public static final int notification_template_icon_bg=0x7f020048;
+        public static final int refresh=0x7f020047;
     }
     public static final class id {
         public static final int action0=0x7f0c0052;
